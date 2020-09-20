f10e3ab0ac444930ee01703b81853adc32624126
“Lauren <lucerolaurenm@gmail.com>
2020-09-20 17:36:02 -0400

    Add Welcome component and test files

---
+++ src/Components/Welcome/Welcome.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Welcome = () => {
+  return (
+    <div>
+      <h2>Welcome to BSM!</h2>
+      <h3>
+        Our mission is to share books by Black authors where readers can find
+        mirrors and windows.
+      </h3>
+      <h4>What are mirrors and windows in books?</h4>
+      <p>
+        Mirrors reflect a reader's own life. Seeing yourself, your family and
+        your culture being valued in the world of a book provides a powerful
+        sense of belonging.
+      </p>
+      <p>
+        Books that are windows offer views into other experiences. Windows teach
+        people to understand and appreciate differences.
+      </p>
+      <p>
+        Please support the Black authors you find here by buying their books,
+        requesting them at libraries, or donating copies to schools and
+        community centers!
+      </p>
+    </div>
+  );
+};
+
+export default Welcome;
---
+++ src/Components/Welcome/Welcome.test.js
@@ -0,0 +1,18 @@
+import "@testing-library/jest-dom/extend-expect";
+import { cleanup, render } from "@testing-library/react";
+import { MemoryRouter as Router } from "react-router-dom";
+import Welcome from "./Welcome";
+import React from "react";
+
+describe("Welcome", () => {
+  afterEach(cleanup);
+
+  it("should render Welcome page", () => {
+    const { getByText } = render(
+      <Router>
+        <Welcome />
+      </Router>
+    );
+    expect(getByText("Welcome to BSM!")).toBeInTheDocument();
+  });
+});
